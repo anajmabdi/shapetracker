c313f2712bb40af73b2e6a86c554b5091340a210
Najma Abdi <anajmabdi@gmail.com>
2022-09-28 14:49:08 -0700

    add css loaders, html plugin

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader']}]
+  },
+};
